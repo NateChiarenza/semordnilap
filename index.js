3fd0da3dc57172acbccb738691f734059d68582d
Nate <>
2021-06-30 08:53:35 -0400

    updated

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const fs = require('fs');
-
-
-
 var dictFile = "words.txt";
 var dict = {};
 
 fs.readFileSync(dictFile)
   .toString()
-  .split('\n')
+  .split('\n') 
   .forEach(function(word) {
     dict[word] = word.split("").reverse().join("");
   });
 
-  function isSemordnilap(word) { return dict[dict[word]]; };
+  function isSemordnilap(word) { 
+    return dict[dict[word]]; 
+  }
  
 var semordnilaps = []
 for (var key in dict) {
@@ -26,15 +25,8 @@ for (var key in dict) {
  
 var count = semordnilaps.length;
 console.log("There are " + count + " semordnilaps in " +
-         dictFile + ".  Here are 5:" );
+         dictFile);
  
-var indices=[]
 for (var i=0; i<count; ++i) {
-  if (Math.random() < 1/Math.ceil(i/5.0)) {
-    indices[i%5] = i 
-  }
-}
-indices.sort()
-for (var i=0; i<5; ++i) {
-  console.log(semordnilaps[indices[i]]);
+  console.log(semordnilaps[i]);
 }
